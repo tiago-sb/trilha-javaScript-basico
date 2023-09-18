4d899f8723bdab369e4534840c52bb822bf37e24
tiago-sb <belatiago72@gmail.com>
2023-09-18 19:56:35 -0300

    inicia o projeto de calculadora com javascript

---
+++ projeto-de-calculadora/scripts.js
@@ -0,0 +1,15 @@
+// declarativas
+function funcao(){
+    console.log("gerada por uma função declarativa")
+}
+funcao()
+
+// atribuição à variável 
+var receberFuncao = function() {
+    
+}
+
+// arrow function
+var funcao_1 = () => {
+    console.log("sou uma arrow function")
+}
