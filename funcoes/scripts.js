598caf4d96a94c8b374d7652ddbb533d9c7722bb
tiago-sb <belatiago72@gmail.com>
2023-09-18 19:05:13 -0300

    adiciona o módulo das funções ao repositório

---
+++ funcoes/scripts.js
@@ -0,0 +1,9 @@
+// function funcao(){
+//     console.log("essa mensagem veio de uma função")
+// }
+// funcao()
+
+function mensagem(primeiro, segundo){
+    console.log(primeiro + "  " + segundo)
+}
+mensagem(primeiro = "tudo certo", segundo = "jovem")
