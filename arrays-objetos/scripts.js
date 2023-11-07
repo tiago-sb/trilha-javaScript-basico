90063b79441565d11bce94c772a8bacf9c70d557
tiago-sb <belatiago72@gmail.com>
2023-11-07 16:49:18 -0300

    adiciona novos métodos de manipulação em array

--- arrays-objetos/scripts.js
+++ arrays-objetos/scripts.js
@@ -1,11 +1,16 @@
-var vetor = ["tiago", 19, 1.78, 'M'] // declaração do array
+var vetor = ["tiago", 19, 1.71, 'M'] // declaração do array
 for(let i = 0; i < vetor.length; i++){
     console.log(vetor[i])
 }
+vetor.pop()
 vetor.forEach(function(item){console.log(item)})
-let vetor2 = [1, 2, 3, 4, 5]
-vetor2.pop()
-vetor2.forEach(function(item){console.log(item)})
+
+let vetor2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let somatorio = 0
+vetor2.forEach(function(item){
+    somatorio += item
+})
+console.log(somatorio)
 
 var xicara = {
     cor: 'branca',
@@ -14,3 +19,7 @@ var xicara = {
 }
 console.log(xicara)
 console.log(xicara.cheia)
+
+const alunos = ["tiago", "julia", "marcos", "felipe junior", "marlos"]
+alunos.push("thiago")
+alunos.forEach(function(alunos, numero){console.log(`aluno ${numero + 1}: ${alunos}`)})
